37ed2de7a37cae13b02b7ac6a247ab955919770c
binhfoto <naibinh@gmail.com>
2018-10-31 18:16:24 +0700

    commit 05: setup material-ui and gdc-react-components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import '@gooddata/react-components/styles/css/main.css';
+import { Visualization } from '@gooddata/react-components';
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
 }
 
 export default App;
+/*
+<Visualization
+        projectId="jroecoqa7jywstxy1hxp8lwl2c4nc10t"
+        identifier="aabUmGwtf2tc"
+    />
+*/
