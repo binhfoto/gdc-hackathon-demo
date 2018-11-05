a4fda3bd25cb7cbe33355e26da52d5ccbc66ead9
binhfoto <naibinh@gmail.com>
2018-11-05 13:40:39 +0700

    commit 10: update README.md

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gdc-hackathon-demo
-test
+
+### Prerequisite
+1. Node 8.10.0
+2. Yarn 1.5.1
+
+### How to setup
+1. `git clone https://github.com/binhfoto/gdc-hackathon-demo.git`
+2. `cd gdc-hackathon-demo`
+3. `yarn install --pure-lockfile`
+4. Open `setupProxy.js`, update `hostname` to target backend
+5. Open `mock-data.index.js`, input project id and AD report uri
+6. `HTTPS=true yarn start`
+7. Browse to `https://localhost:3000/account.html` and input credential
+
+### How to use
+... 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,29 +1,31 @@
 const proxy = require('http-proxy-middleware');
 
+const hostname = 'staging3.intgdc.com';
+
 module.exports = function (app) {
      app.use(proxy("/gdc", {
          "changeOrigin": true,
          "cookieDomainRewrite": "localhost",
          "secure": false,
-         "target": "https://staging3.intgdc.com/",
+         "target": `https://${hostname}/`,
          "headers": {
-             "host": "staging3.intgdc.com",
+             "host": `${hostname}`,
              "origin": null
          }
      }));
      app.use(proxy("/*.html", {
          "changeOrigin": true,
          "secure": false,
-         "target": "https://staging3.intgdc.com/"
+         "target": `https://${hostname}/`
      }));
      app.use(proxy("/packages/*.css", {
          "changeOrigin": true,
          "secure": false,
-         "target": "https://staging3.intgdc.com/"
+         "target": `https://${hostname}/`
      }));
      app.use(proxy("/packages/*.js", {
          "changeOrigin": true,
          "secure": false,
-         "target": "https://staging3.intgdc.com/"
+         "target": `https://${hostname}/`
      }));
  };
