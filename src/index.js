b01f42ee51192b11e86b26fa18bd93d7884e1ba0
Tan Nguyen <duytan1008@gmail.com>
2018-11-06 17:28:15 +0700

    add user menu on header

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import './index.css';
 //import App from './App';
 import Dashboard from './components/Dashboard';
 import * as serviceWorker from './serviceWorker';
+import User from "./services/user.service";
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+User.getStatus()
+    .then(isActive => {
+        if (isActive) {
+            ReactDOM.render(<Dashboard />, document.getElementById('root'));
+        } else {
+            window.location.href = 'account.html';
+        }
+    });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
