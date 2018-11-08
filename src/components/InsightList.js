84fe914ae2d8e267b21c52dd04d3ce1643413ada
binhfoto <naibinh@gmail.com>
2018-11-08 11:42:09 +0700

    commit 18: use PureComponent to improve render performance

--- src/components/InsightList.js
+++ src/components/InsightList.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Grid from '@material-ui/core/Grid';
 import InsightItem from './InsightItem';
 
-
-const InsightList = ({projectId, insights, onEdit, onDelete}) => {
-    return (
-        <Grid container spacing={16}>
-            {insights.map(({key, uri, identifier}) => (
-                <InsightItem
-                    key={key}
-                    uri={uri}
-                    identifier={identifier}
-                    projectId={projectId}
-                    onEdit={onEdit}
-                    onDelete={onDelete}
-                />
-            ))}
-        </Grid>
-    );
-};
+// use PureComponent to avoid re-render 'Visualization'
+class InsightList extends PureComponent {
+    render() {
+        const {projectId, insights, onEdit, onDelete} = this.props;
+        return (
+            <Grid container spacing={16}>
+                {insights.map(({key, uri, identifier}) => (
+                    <InsightItem
+                        key={key}
+                        uri={uri}
+                        identifier={identifier}
+                        projectId={projectId}
+                        onEdit={onEdit}
+                        onDelete={onDelete}
+                    />
+                ))}
+            </Grid>
+        );
+    }
+}
 
 export default InsightList;
